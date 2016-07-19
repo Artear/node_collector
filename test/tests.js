0db255e939b7bf28fecc3c1f0e731b146ad79d0c
Martin Moreno <155martinmoreno@gmail.com>
2016-07-19 17:08:57 -0300

    Added: collector get data; Added: subscriptor get data;

--- test/tests.js
+++ test/tests.js
@@ -1,31 +1,31 @@
+const test_app_name = 'test_mam';
+const test_app_version = 1;
+
+
 var assert = require('chai').assert;
-var mockery = require('mockery');
 var fakeredis = require('fakeredis');
 var Collector = require('../lib/collector');
 var Subscriber = require('../lib/subscriber');
 
-
-var fake_redis_ip = '127.0.0.1';
-var fake_redis_port = 6379;
-var test_app_name = 'test_mam';
-var test_app_version = 1;
-
-
 describe('Testing Collector', function () {
-    before(function () {
-        mockery.enable();
-        mockery.registerMock('redis', fakeredis);
+
+    var redis_client = null;
+    var redis_client_sub = null;
+
+    beforeEach(function () {
+        var redis_name = "random_" + Math.random() * 9999999;
+        redis_client = fakeredis.createClient(redis_name, {fast: true});
+        redis_client_sub = fakeredis.createClient(redis_name, {fast: true});
     });
 
     it('Should collect 5 times', function (done) {
         var call_count = 0;
 
         var collector = new Collector(
-            fake_redis_ip,
-            fake_redis_port,
+            redis_client,
             test_app_name,
             test_app_version,
-            50,
+            5,
             function (saveFn) {
                 call_count++;
 
@@ -41,11 +41,10 @@ describe('Testing Collector', function () {
         var call_count = 0;
 
         var collector = new Collector(
-            fake_redis_ip,
-            fake_redis_port,
+            redis_client,
             test_app_name,
             test_app_version,
-            50,
+            5,
             function (saveFn) {
                 call_count++;
                 collector.stopCollecting();
@@ -58,7 +57,7 @@ describe('Testing Collector', function () {
                     }
 
                     clearTimeout(timeout);
-                }, 100);
+                }, 10);
             });
 
         collector.startCollecting();
@@ -69,8 +68,7 @@ describe('Testing Collector', function () {
         var test_data = "Un Cohete a la Luna";
 
         var collector = new Collector(
-            fake_redis_ip,
-            fake_redis_port,
+            redis_client,
             test_app_name,
             test_app_version,
             50,
@@ -79,21 +77,94 @@ describe('Testing Collector', function () {
             }
         );
 
+        var call_count = 0;
+
         var subscriber = new Subscriber(
-            fake_redis_ip,
-            fake_redis_port,
+            redis_client,
+            redis_client_sub,
             test_app_name,
             test_app_version,
             function (data) {
-                assert.equal(test_data, data);
-                done();
+                if (call_count == 0) {//only execute 1 time
+                    call_count++;
+                    assert.equal(test_data, data);
+                    collector.stopCollecting();
+
+                    done();
+                }
             });
 
         subscriber.startListening();
         collector.startCollecting();
     });
 
-    after(function () {
-        mockery.disable();
+    it('Collector Should get saved value', function (done) {
+        var test_key = "key";
+        var test_data = "Un Cohete a la Luna";
+
+        var collector = new Collector(
+            redis_client,
+            test_app_name,
+            test_app_version,
+            5,
+            function (saveFn) {
+                collector.stopCollecting();
+
+                saveFn(test_key, test_data, 50000, function (err, reply) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    collector.getValue(test_key, function (err, value) {
+                        if (err) {
+                            throw err;
+                        }
+
+                        assert.equal(test_data, value);
+                        done();
+                    });
+                });
+            }
+        );
+
+        collector.startCollecting();
     });
+
+    it('Subscriber Should get saved value', function (done) {
+            var test_key = "key";
+            var test_data = "Un Cohete a la Luna";
+
+            var collector = new Collector(
+                redis_client,
+                test_app_name,
+                test_app_version,
+                50,
+                function (saveFn) {
+                    saveFn(test_key, test_data, 5000);
+                }
+            );
+
+            var subscriber = new Subscriber(
+                redis_client,
+                redis_client_sub,
+                test_app_name,
+                test_app_version,
+                function (data) {
+                    collector.stopCollecting();
+
+                    subscriber.getValue(test_key, function (err, value) {
+                        if (err) {
+                            throw err;
+                        }
+
+                        assert.equal(test_data, value);
+                        done();
+                    });
+                });
+
+            subscriber.startListening();
+            collector.startCollecting();
+        }
+    );
+
 });
