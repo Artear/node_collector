e5fd60b41355f374355500d6dd298b8427078130
Martin Moreno <155martinmoreno@gmail.com>
2016-07-20 11:38:35 -0300

    Exposed logger;

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var Collector = require("./lib/collector");
 var Subscriber = require("./lib/subscriber");
+var logger = require("./helper/logger");
 
 module.exports = {
     Collector: Collector,
-    Subscriber: Subscriber
-}
-;
+    Subscriber: Subscriber,
+    logger: logger
+};
